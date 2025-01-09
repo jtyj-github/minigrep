dee2938853b6a32bf0ae26cbdea61d895cc45e2a
jtyj-github <tanyjjustin@gmail.com>
2025-01-09 17:15:12 +0800

    ⚡️feat: added simple arg collection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Converts iterator of args inro a collection (Vector)
+    // Only accept valid Unicode, else panic
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 }
